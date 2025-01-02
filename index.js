168e91b98d96fa035098dbabd766a53a54f813da
rajiul93 <rajiulislam283@gmail.com>
2025-01-02 19:43:21 +0600

    first commit

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 
 
 const corsOptions = {
-  origin: ["http://localhost:5173", "http://localhost:5174"],
+  origin: ["http://localhost:5173", "http://localhost:5174","https://kodomtolajamemosque.netlify.app"],
   credentials: true,
   optionSuccessStatus: 200,
 };
